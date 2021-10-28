0a3f39b188f3e50967e04049c60054135b3f79e6
Pelumi527 <fatolupelumi@gmail.com>
2021-10-28 15:12:28 +0100

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Layout from './components/Layout';
 import Home from './pages/Home'
 import About from './pages/About'
+import Roadmap from './pages/Roadmap';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
+      <Route exact path="/road-map" component={Roadmap} />
     </Switch>
     </Layout>
   );
