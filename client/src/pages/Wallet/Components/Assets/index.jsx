14688feb4b1addb0a68e71a586b5fd23ab3e5962
Pels <fatolupelumi@gmail.com>
2021-11-10 07:35:34 +0100

    added the plus logo

--- client/src/pages/Wallet/Components/Assets/index.jsx
+++ client/src/pages/Wallet/Components/Assets/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Row, Col, Image } from 'react-bootstrap'
 import {  AddBtn, AssetDiv, P } from './style';
+import Plus from "../../../../assets/plus.png"
 
 
 function Assets() {
@@ -17,7 +18,7 @@ function Assets() {
                          <h5>USD 20220.21</h5>
                  </Col>
                  <Col lg="2" md="2" sm="2">
-                     <AddBtn><Image src={`images/plus.png`} fluid/></AddBtn>
+                     <AddBtn><Image src={Plus} fluid/></AddBtn>
                  </Col>
              </Row>
         </AssetDiv>
